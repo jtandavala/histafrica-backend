731a69db33a1e285e4387c8c5ab7efe40be42ace
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-08-28 20:29:28 +0100

    :sparkles: (ValueObject) make it immutable

--- src/histafrica/shared/tests/unit/domain/test_value_objects.py
+++ src/histafrica/shared/tests/unit/domain/test_value_objects.py
@@ -1,6 +1,6 @@
 import unittest
 from abc import ABC
-from dataclasses import dataclass, is_dataclass
+from dataclasses import FrozenInstanceError, dataclass, is_dataclass
 
 from histafrica.shared.domain.value_objects import ValueObject
 
@@ -24,6 +24,13 @@ class TestValueObjectUnit(unittest.TestCase):
     def test_if_is_a_abstract_class(self):
         self.assertIsInstance(ValueObject(), ABC)
 
+    def test_convert_to_string(self):
+        vo1 = StubOneProp(prop="value")
+        self.assertEqual(vo1.prop, str(vo1))
+
+        vo2 = StubTwoProp(prop1="value1", prop2="value2")
+        self.assertEqual('{"prop1": "value1", "prop2": "value2"}', str(vo2))
+
     def test_init_prop(self):
         vo1 = StubOneProp(prop="value")
         self.assertEqual(vo1.prop, "value")
@@ -31,3 +38,8 @@ class TestValueObjectUnit(unittest.TestCase):
         vo2 = StubTwoProp(prop1="value1", prop2="value2")
         self.assertEqual(vo2.prop1, "value1")
         self.assertEqual(vo2.prop2, "value2")
+
+    def test_is_immutable(self):
+        with self.assertRaises(FrozenInstanceError):
+            value_object = StubOneProp(prop="value")
+            value_object.prop = "fake"
