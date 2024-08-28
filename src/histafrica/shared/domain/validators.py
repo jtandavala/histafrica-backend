8d6b23b96095461069bc5da987364b2fce78ee44
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-08-28 21:25:57 +0100

    :bug: (Validator) fix typos

--- src/histafrica/shared/domain/validators.py
+++ src/histafrica/shared/domain/validators.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Any, Dict, List
 
+from .exceptions import ValidationException
+
 ErrorFields = Dict[str, List[str]]
 
 
@@ -12,3 +14,8 @@ class ValidatorRules:
     @staticmethod
     def values(value: Any, prop: str):
         return ValidatorRules(value, prop)
+
+    def required(self) -> "ValidatorRules":
+        if self.value is None or self.value == "":
+            raise ValidationException(f"The {self.prop} must be a string")
+        return self
--- src/histafrica/shared/tests/unit/domain/test_validator.py
+++ src/histafrica/shared/tests/unit/domain/test_validator.py
@@ -1,5 +1,6 @@
 import unittest
 
+from histafrica.shared.domain.exceptions import ValidationException
 from histafrica.shared.domain.validators import ValidatorRules
 
 
@@ -10,3 +11,12 @@ class TestValidatorRules(unittest.TestCase):
         self.assertIsInstance(validator, ValidatorRules)
         self.assertEqual(validator.value, "some value")
         self.assertEqual(validator.prop, "prop")
+
+    def test_requred_rule(self):
+        invalid_data = [{"value": None, "prop": "prop"}, {"value": "", "prop": "prop"}]
+
+        for i in invalid_data:
+            msg = f'value: {i["value"]}, prop: {i["prop"]}'
+            with self.assertRaises(ValidationException, msg=msg) as assert_error:
+                ValidatorRules.values(i["value"], i["prop"]).required()
+                self.assertEqual("The prop is required", assert_error.exception.args[0])
