7599d2f289daad1273b0276c987c338538a0696c
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-08-29 19:56:00 +0100

    :bug: (isort) fix

--- src/histafrica/shared/domain/validators.py
+++ src/histafrica/shared/domain/validators.py
@@ -1,9 +1,11 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Any, Dict, List
+from typing import Any, Dict, Generic, List, TypeVar
 
 from .exceptions import ValidationException
 
 ErrorFields = Dict[str, List[str]]
+PropsValidated = TypeVar("PropsValidated")
 
 
 @dataclass(frozen=True, slots=True)
@@ -17,5 +19,32 @@ class ValidatorRules:
 
     def required(self) -> "ValidatorRules":
         if self.value is None or self.value == "":
+            raise ValidationException(f"The {self.prop} is required")
+        return self
+
+    def string(self) -> "ValidatorRules":
+        if self.value is not None and not isinstance(self.value, str):
             raise ValidationException(f"The {self.prop} must be a string")
         return self
+
+    def max_length(self, max_length: int) -> "ValidatorRules":
+        if self.value is not None and len(self.value) > max_length:
+            raise ValidationException(
+                f"The {self.prop} must be less than {max_length} characters"
+            )
+        return self
+
+    def boolean(self) -> "ValidatorRules":
+        if self.value is not None and not isinstance(self.value, bool):
+            raise ValidationException(f"The {self.prop} must be a boolean")
+        return self
+
+
+@dataclass(slots=True)
+class ValidatorFieldsInterface(ABC, Generic[PropsValidated]):
+    errors: ErrorFields = None
+    validated_data: PropsValidated = None
+
+    @abstractmethod
+    def validate(self, data: Any) -> bool:
+        raise NotImplementedError()
