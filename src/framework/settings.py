823cba576b79920c6a0334c33e732a5443b38ddc
jose.tandavala <jose.tandavala@ideiasdinamicas.com>
2024-09-03 19:12:14 +0100

    :sparkles: (SAST) add .isort file

--- src/framework/settings.py
+++ src/framework/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
-# chage here
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
